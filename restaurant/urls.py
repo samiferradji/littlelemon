d734fcaf8eb7675e81e58d4d955b7ad1d51cc2e4
sami <git config user.email 3h1qwo2p1w364k90ouxurr3foihj7w@bots.bitbucket.org>
2024-02-06 14:17:32 +0100

    Add Menu and booking API endpoints

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .views import MenuListCreateApi, MenuRetrieveUpdateDestroyApi
+from .views import router
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    # Menu urls
+    path('menu', MenuListCreateApi.as_view(), name='menu_list_create'),
+    path('menu/<int:pk>', MenuRetrieveUpdateDestroyApi.as_view(), name='menu_retrieve_update_destroy'),
+    # Booking urls
+    path('booking/', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.routers import DefaultRouter
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+class MenuListCreateApi(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class MenuRetrieveUpdateDestroyApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+router = DefaultRouter()
+router.register(r'tables', BookingViewSet)
